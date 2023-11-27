3254f875fad342a3266892880c5a0de6e6c9fc32
Muhammedjazimtk <77974989+Muhammedjazimtk@users.noreply.github.com>
2023-11-27 20:46:05 +0530

    time bug fixed

--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -3,27 +3,52 @@
 // eslint-disable-next-line react/prop-types
 const RepoCard = ({ name, language, update, url }) => {
   let timeString = "";
-  let strDate = update;
-  strDate = strDate.substring(0, 10);
-  const date1 = new Date(strDate);
+  // let strDate = update;
+  // strDate = strDate.substring(0, 10);
+  const date1 = new Date(update);
   const date2 = new Date();
   const diffTime = Math.abs(date2 - date1);
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-  //   console.log(diffTime + " milliseconds");
-  //   console.log(diffDays + " days");
+  console.log(diffTime + " milliseconds" + "for " + name);
+  console.log(diffDays + " days" + "for " + name);
+  // console.log(date1 + " " + name);
   const month = date1.toLocaleString("default", { month: "short" });
+  const year = date1.toLocaleString("default", { year: "numeric" });
+  const day = date1.toLocaleString("default", { day: "numeric" });
 
-  if (diffDays > 32) {
-    timeString =
-      "updated on " +
-      month +
-      " , " +
-      date1.getDay() +
-      " , " +
-      date1.getFullYear();
+  if (diffTime < 86400000) {
+    if (diffTime < 3600000) {
+      let min = Math.ceil(diffTime / 60000);
+      timeString = "updated " + min + " minutes ago";
+    } else {
+      let h = Math.floor(diffTime / 3600000);
+
+      timeString = "updated " + h + " hours ago";
+    }
   } else {
-    timeString = "updated " + diffDays + " days ago";
+    let days = Math.ceil(diffTime / 86400000);
+    if (days < 30) {
+      if (days == 1) {
+        timeString = "updated " + days + " day ago";
+      } else {
+        timeString = "updated " + days + " days ago";
+      }
+    } else {
+      timeString = "updates on " + month + " " + day + " , " + year;
+    }
   }
+
+  // if (diffDays > 32) {
+  //   timeString =
+  //     "updated on " +
+  //     month +
+  //     " , " +
+  //     date1.getDay() +
+  //     " , " +
+  //     date1.getFullYear();
+  // } else {
+  //   timeString = "updated " + diffDays + " days ago";
+  // }
 
   return (
     <div className="flex flex-col justify-between p-2 gap-6  border border-gray-600 rounded-md w-full h-full">
